5df7095db7b3475f19305e5b9f5c81a98d8c5aff
ajudantedemagico <jqlngnclvz@gmail.com>
2026-05-30 15:55:45 -0300

    feat(database): funcionalidades do banco de dados (SQlite)

---
+++ src/database/database.js
@@ -0,0 +1,28 @@
+import * as SQLITE from 'expo-sqlite';
+
+const db = SQLITE.openDatabaseSync('budgie.db');
+
+export const initDatabase =() => {
+    db.execSync(`
+        CREATE TABLE IF NOT EXISTS gastos (
+         id INTEGER PRIMARY KEY AUTOINCREMENT,
+         descricao TEXT NOT NULL,
+         categoria TEXT NOT NULL
+         valor REAL NOT NULL
+         data TEXT NOT NULL
+    );
+    `);
+};
+
+export const inserirGastos = (gasto) => {
+    db.runSync(
+        'INSERT INTO gastos (descricao, categoria, valor, data) VALUES (?,?,?,?)',
+        [gastos.descricao, gastos.categoria, gastos.valor, gastos.data]
+    );
+};
+
+export const listarGastos =() => {
+    return db.getAllSync ('SELECT * FROM gastos ORDER BY id DESC')
+};
+
+export default db;
